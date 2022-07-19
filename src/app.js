f577bdf1cdeaa86bb91c4de4094ad8988fcdabfd
Olha Podorozhnaya <opn2606@gmail.com>
2022-07-19 08:02:11 +0300

    show current time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
+function formatDate(timestemp) {
+  let date = new Date(timestemp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = weekDays[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  } 
+   if (hours < 10) {
+     hours = `0${hours}`;
+   } 
+  return `${day} ${hours}:${minutes}`;
+}
+
+
 function showTemp(response) {
   console.log(response.data)
   let temperatureElement = document.querySelector("#temperature");
@@ -5,12 +21,17 @@ function showTemp(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt*1000);
+
 }
+
+
 
 
 let apiKey = "0c669309e9b69198d164920a0d742074"; 
