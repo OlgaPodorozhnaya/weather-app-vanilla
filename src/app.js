e29f9f6b7fb25ad53b9f54a6669e14e6ce891155
Olha Podorozhnaya <opn2606@gmail.com>
2022-07-26 05:05:57 +0300

    edited forecast

--- src/app.js
+++ src/app.js
@@ -13,31 +13,43 @@ function formatDate(timestemp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formateDay(timestemp) {
+  let date = new Date(timestemp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+  
+}
+
 
 function displayForecast(response) {
-  console.log(response.data.daily);
+  // console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  // let days = ["Thu", "Fri", "Sat", "Sun"];
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formateDay(forecastDay.dt)}</div>
 
                 <img
-                  src="http://openweathermap.org/img/wn/03d@2x.png"
+                  src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
                   alt=""
                   width="46px"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">36°</span>
-                  <span class="weather-forecast-temperature-min">19°</span>
+                  <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temp.max)}°</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temp.min)}°</span>
                 </div>
               </div> 
           `;
+    };
   })
   
 
