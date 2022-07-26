3adb353079c3fc7a560bb70b841afbf775571783
Olha Podorozhnaya <opn2606@gmail.com>
2022-07-26 04:31:39 +0300

    added forecast and got access to forecast data

--- src/app.js
+++ src/app.js
@@ -14,7 +14,8 @@ function formatDate(timestemp) {
 }
 
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -44,8 +45,17 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+ function getForecast(coordinates) {
+   console.log(coordinates);
+  //  0c669309e9b69198d164920a0d742074
+   let apiKey = "0c669309e9b69198d164920a0d742074";
+   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+   console.log(apiUrl);
+   axios.get(apiUrl).then(displayForecast);
+ }
+
 function showTemp(response) {
-  console.log(response.data)
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -63,7 +73,9 @@ function showTemp(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt*1000);
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
-iconElement.setAttribute("alt", response.data.weather[0].description)
+  iconElement.setAttribute("alt", response.data.weather[0].description)
+  
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -109,4 +121,4 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", changefahrenheit);
 
 search("New York");
-displayForecast();
+
